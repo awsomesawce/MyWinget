71ac97deb76e140769bfbb3716db3fb6749607d3
Carl Capodice <69673640+awsomesawce@users.noreply.github.com>
2024-05-11 15:58:15 -0400

    fix: :art: Added docs, added functionality
    Added actual functionality of the function.  Also included a super simple implementation which is not exported.

--- MyWinget.psm1
+++ MyWinget.psm1
@@ -6,8 +6,22 @@ Show winget packages one after the other
 .DESCRIPTION
 This function is an upgrade of the simple one I use on the commandline.
 It uses `ValueFromRemainingArguments` because I don't like to separate args with commas.
+.PARAMETER PkgNames
+Array of strings of names of packages to look up on winget
+.PARAMETER OutFile
+File to output information to.
+.PARAMETER PassThru
+Whether or not to display items on commandline when paired with $OutFile
+.FUNCTIONALITY
+convenience
+.EXAMPLE
+Show-Winget -OutFile pkginfo.txt Microsoft.Powershell KDE.Kate
+
+Prints package info to pkginfo.txt
 .NOTES
 I will include a simple version in this module for demonstration purposes.
+
+TODO: Add more functionality
 #>
     [CmdletBinding()]
     param(
@@ -18,16 +32,47 @@ I will include a simple version in this module for demonstration purposes.
         [Parameter(Mandatory = $false)]
         [ValidateNotNullOrEmpty()]
         [string]
-        $OutFile
+        $OutFile,
+
+        [Parameter()]
+        [switch]
+        $PassThru
     )
     Write-Debug "`$PkgNames: $PkgNames"
     Write-Debug "Type: $($PkgNames.GetType())"
     if ($OutFile) {
-        Write-Debug "`$Outfile: $OutFile"
+        if (Test-Path $OutFile) {
+            # ! File must not exist
+            Write-Error "File already exists at $OutFile"
+            throw "ERROR: File exists"
+        }
+        Write-Debug "Writing to `$outfile $outfile"
+        
+        foreach ($item in $PkgNames) {
+            if ($PassThru) {
+                Write-Verbose "Looking up $item"
+                winget show $item | Tee-Object -FilePath $OutFile -Append -Encoding utf8
+            } else {
+                winget show $item | out-file -FilePath $OutFile -Encoding utf8 -Append -NoClobber
+            }
+        }
     }
     else {
-        write-debug "false"
+        write-debug "no outfile"
+        
+        $PkgNames | ForEach-Object {
+            Write-Verbose "Looking up $_"
+            winget show $_
+        }
+        Write-Verbose "Done"
     }
+}
+
+function Show-WingetSimple {
+    <#.DESCRIPTION
+    Simple implementation of this function.
+    #>
+    foreach ($i in $args) {winget show $i}
 }
 
 Export-ModuleMember -Function 'Show-Winget'
