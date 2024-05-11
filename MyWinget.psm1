f840a0ce0c2721a046486770c0deb3c8fb82b5ee
Carl Capodice <69673640+awsomesawce@users.noreply.github.com>
2024-05-11 15:38:03 -0400

    docs: :tada: Added description and notes
    Added description and notes to Show-Winget function

--- MyWinget.psm1
+++ MyWinget.psm1
@@ -1,26 +1,33 @@
 function Show-WinGet {
 
-<#
+    <#
 .SYNOPSIS
 Show winget packages one after the other
+.DESCRIPTION
+This function is an upgrade of the simple one I use on the commandline.
+It uses `ValueFromRemainingArguments` because I don't like to separate args with commas.
+.NOTES
+I will include a simple version in this module for demonstration purposes.
 #>
-[CmdletBinding()]
-param(
-[Parameter(Mandatory=$true, ValueFromRemainingArguments=$true, Position=0, ValueFromPipelineByPropertyName)]
-[string[]]
-$PkgNames,
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromRemainingArguments = $true, Position = 0, ValueFromPipelineByPropertyName)]
+        [string[]]
+        $PkgNames,
 
-[Parameter(Mandatory=$false)]
-[ValidateNotNullOrEmpty()]
-[string]
-$OutFile
-)
-Write-Debug "`$PkgNames: $PkgNames"
-Write-Debug "Type: $($PkgNames.GetType())"
-if ($OutFile) {
-Write-Debug "`$Outfile: $OutFile"
-} else {
-write-debug "false"
-}
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $OutFile
+    )
+    Write-Debug "`$PkgNames: $PkgNames"
+    Write-Debug "Type: $($PkgNames.GetType())"
+    if ($OutFile) {
+        Write-Debug "`$Outfile: $OutFile"
+    }
+    else {
+        write-debug "false"
+    }
 }
 
+Export-ModuleMember -Function 'Show-Winget'
